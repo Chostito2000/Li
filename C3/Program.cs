2447e64c2cf07b87adb1e06d78787d082be28e19
Alumno <Alumno@LAB102-02>
2023-10-02 10:58:10 -0500

    Agregar archivos de proyecto.

---
+++ C3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Animal> animales = new List<Animal>();
+        animales.Add(new Animal() { Nombre = "Hormiga", Color = "Rojo" });
+        animales.Add(new Animal() { Nombre = "Lobo", Color = "gris" });
+        animales.Add(new Animal() { Nombre = "Elefante", Color = "gris" });
+        animales.Add(new Animal() { Nombre = "Pantera", Color = "negro" });
+        animales.Add(new Animal() { Nombre = "Gato", Color = "negro" });
+        animales.Add(new Animal() { Nombre = "Iguana", Color = "verde" });
+        animales.Add(new Animal() { Nombre = "Sapo", Color = "verde" });
+        animales.Add(new Animal() { Nombre = "Camaleon", Color = "verde" });
+        animales.Add(new Animal() { Nombre = "Gallina", Color = "blanco" });
+
+        var animalesOrdenadosAlfabeticamente = animales.OrderBy(animal => animal.Nombre).ToList();
+
+        Console.WriteLine("Animales ordenados alfabéticamente por Nombre:");
+        foreach (var animal in animalesOrdenadosAlfabeticamente)
+        {
+            Console.WriteLine($"Nombre: {animal.Nombre}, Color: {animal.Color}");
+        }
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("att. Flores Gonzales Brayan Wilson");
+    }
+    public class Animal
+    {
+        public string Nombre { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using lin;
+
+Linqueries queries = new Linqueries();
+
+//Imprimir toda la lista de libros
+ImprimeValores(queries.TodaLaColeccion());
+
+
+void ImprimeValores(IEnumerable<Book> listaDeLibros)
+{
+    Console.WriteLine("{0, -70} {1, 15} {2, 15}", "Titulo", "N. Paginas", "Fecha Publicacion");
+    foreach (var item in listaDeLibros)
+    {
+        Console.WriteLine(
+            "{0, -70} {1, 15} {2, 15}",
+            item.Title,
+            item.PageCount,
+            item.PublishedDate
+            );
+    }
+}
