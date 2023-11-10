15429c168920dd084740a40539e2e39bf3e0a933
Brayan <floresgonzalesb@gmail.com>
2023-11-10 09:24:33 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use App\Http\Controllers\RegistroProductowebcontroller;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
