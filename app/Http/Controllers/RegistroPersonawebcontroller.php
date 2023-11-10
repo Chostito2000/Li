8bdf11569e6fcdbd895aad0315c29f0df811a459
Soporte <Soporte@LAB102-02>
2023-11-10 12:22:35 -0500

    update

--- app/Http/Controllers/RegistroPersonawebcontroller.php
+++ app/Http/Controllers/RegistroPersonawebcontroller.php
@@ -16,21 +16,23 @@ class RegistroPersonawebcontroller extends Controller
     public function guardarPersona(Request $request){
         try{
             $data = [
-                'nombres'=> $request->get('nombres'),
-                'paterno'=> $request->get('paterno'),
-                'materno'=> $request->get('materno'),
-                'bibliografia'=> $request->get('bibliografia'),
-                'foto'=> $request->get('foto'),
-                'documento'=> $request->get('documento'),
-                'celular'=> $request->get('celular'),
+                'nombres' => $request->get('nombres'),
+                'paterno' => $request->get('paterno'),
+                'materno' => $request->get('materno'),
+                'bibliografia' => $request->get('bibliografia'),
+                'foto' => $request->get('foto'),
+                'documento' => $request->get('documento'),
+                'celular' => $request->get('celular'),
             ];
 
             // Persona::create($data);
+
             return redirect()->route('lista-personas')
                 ->with('mensaje','Persona registrada correctamente!!!');
             
         } catch(Exception $ex){
-            dd($ex);
+            
+            dd($Exception->getMessage);
             return redirect()->route('registro.persona')
                 ->with('mensaje',$ex->getMessage());
         }  
